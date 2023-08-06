20fafde97602a4ff46899a6a7517b09d37f8e887
Alina Sin <alina.sinelnikov@gmail.com>
2023-08-06 17:14:24 +0300

    all warking no css

--- app.py
+++ app.py
@@ -9,23 +9,28 @@ def home(items=[]):
         items=todict()
     return render_template('home.html', tasks = sorted(items, key = lambda i:i['date']))
 
-@app.route('/sa')
-def sa():
-    action = request.args.get('action')
-    print(action)
-    if action == 'search':
-        category = request.args['category']
-        description = request.args['description']
-        date = request.args['date']
-        results = todict(f'SELECT * FROM tasks WHERE category LIKE "%{category}%" AND description LIKE "%{description}%" AND date LIKE "%{date}%"')
-        return home(items = results)
-            
-    else:
-        category = request.args['category']
-        description = request.args['description']
-        date = request.args['date']
-        query(f"INSERT INTO tasks (category, description, date) VALUES ('{request.args['category']}','{request.args['description']}','{request.args['date']}')")
-        return redirect((url_for('home')))
+@app.route('/search')
+def search():
+    category = request.args['category']
+    description = request.args['description']
+    date = request.args['date']
+    results = todict(f'SELECT * FROM tasks WHERE category LIKE "%{category}%" AND description LIKE "%{description}%" AND date LIKE "%{date}%"')
+    return home(items = results)
+
+@app.route('/postAdd')
+def postAdd():
+    category = request.args['category']
+    description = request.args['description']
+    day = request.args['day']
+    month = request.args['month']
+    year = request.args['year']
+    date = f"{day}/{month}/{year}"
+    query(f"INSERT INTO tasks (category, description, date) VALUES ('{category}','{description}','{date}')")
+    return redirect(url_for('home'))
+
+@app.route('/add')
+def add():        
+    return render_template('add.html')
 
 @app.route('/update')
 def update():
@@ -38,7 +43,10 @@ def post_update():
     tid=request.args["tid"]
     category=request.args["category"]
     description=request.args["description"]
-    date=request.args["date"]
+    day=request.args["day"]
+    month = request.args["month"]
+    year = request.args["year"]
+    date = f"{day}/{month}/{year}"
     query(f"UPDATE tasks SET category='{category}', description='{description}', date='{date}' WHERE tid='{tid}'")
     return redirect(url_for('home'))
 
